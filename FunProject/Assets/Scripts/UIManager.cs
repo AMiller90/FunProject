33cba687da0c0d87b3cfa83f752b600950b33c92
Andrew Miller <A_Miller729@yahoo.com>
2016-10-18 15:29:33 -0500

    Added UIManager text
    Functionality put into game checking for victory as well as UI elements

--- FunProject/Assets/Scripts/UIManager.cs
+++ FunProject/Assets/Scripts/UIManager.cs
@@ -1,35 +1,97 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System;
 
-public sealed class UIManager : MonoBehaviour {
+public sealed class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas P1Canvas;
 
-    private static UIManager instance;
+    [SerializeField]
+    private Canvas P2Canvas;
 
-    public static UIManager self
+    [SerializeField]
+    private Text P1Scoretext;
+    [SerializeField]
+    private Text P2Scoretext;
+
+    [SerializeField]
+    private static Text P1Victorytext;
+    [SerializeField]
+    private static Text P2Victorytext;
+
+    private static float p1Score;
+    private static float p2Score;
+
+    public static float P1Score
     {
         get
         {
-            if (instance == null)
-                instance = new UIManager();
-            return instance;
+            return p1Score;
+        }
+
+        set
+        {
+            p1Score = value;
         }
     }
 
-    [SerializeField]
-    private Canvas UICanvas;
+    public static float P2Score
+    {
+        get
+        {
+            return p2Score;
+        }
 
-    // Use this for initialization
+        set
+        {
+            p2Score = value;
+        }
+    }
+
     void Start ()
     {
-        if (UICanvas == null)
-            UICanvas = GameObject.Find("UICanvas").GetComponent<Canvas>();
+        if (P1Canvas == null)
+            P1Canvas = GameObject.Find("P1Canvas").GetComponent<Canvas>();
+
+        if (P2Canvas == null)
+            P2Canvas = GameObject.Find("P2Canvas").GetComponent<Canvas>();
+
+        if(P1Scoretext == null)
+            P1Scoretext = P1Canvas.GetComponentsInChildren<Text>()[1];
+
+        if(P2Scoretext == null)
+            P2Scoretext = P2Canvas.GetComponentsInChildren<Text>()[1];
+
+        if (P1Victorytext == null)
+            P1Victorytext = P1Canvas.GetComponentsInChildren<Text>()[2];
+
+        if (P2Victorytext == null)
+            P2Victorytext = P2Canvas.GetComponentsInChildren<Text>()[2];
+
+        p1Score = 0;
+        p2Score = 0;
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
-	}
+        P1Scoretext.text = "Score: " + p1Score.ToString();
+        P2Scoretext.text = "Score: " + p2Score.ToString();
+    }
 
+    public static void CheckForVictory()
+    {
+        if (p1Score >= 2)
+        {
+            P1Victorytext.text = "You Win!";
+            P2Victorytext.text = "You Lose!";
+        }
+            
+
+        if (p2Score >= 2)
+        {
+            P2Victorytext.text = "You Win!";
+            P1Victorytext.text = "You Lose!";
+        }
+            
+    }
 }
