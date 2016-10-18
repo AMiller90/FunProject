505abd5b7a7187dddc5cf0c9a1a13c7c2ab297b4
Andrew Miller <A_Miller729@yahoo.com>
2016-10-18 11:50:08 -0500

    Added UIManager, and fixed Ball movement
    UIManager will handle UI elements and the ball now moves as its supposed
    to

--- FunProject/Assets/Scripts/AIBall.cs
+++ FunProject/Assets/Scripts/AIBall.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AIBall : MonoBehaviour {
+public sealed class AIBall : MonoBehaviour {
+
+    private static AIBall instance;
+
+    public static AIBall self
+    {
+        get
+        {
+            if (instance == null)
+                instance = new AIBall();
+            return instance;
+        }
+    }
+
+    private Rigidbody rb;
+    private Rigidbody p1;
+    private Rigidbody p2;
 
     private Transform player1;
     private Transform player2;
+
     // Use this for initialization
     void Start ()
     { 
@@ -16,29 +33,92 @@ public class AIBall : MonoBehaviour {
         if (player2 == null)
             player2 = GameObject.FindGameObjectWithTag("Player2").transform;
 
-        //GetComponent<Rigidbody>().velocity += new Vector3(0, 0, -5);
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -5), ForceMode.Impulse);
+        rb = GetComponent<Rigidbody>();
+        p1 = player1.GetComponent<Rigidbody>();
+        p2 = player1.GetComponent<Rigidbody>();
+
+        StartCoroutine(Pause());
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 currentVelocity = rb.velocity;
+            SpawnBall();
+            rb.velocity = new Vector3(0, 0, 0);
+            StartCoroutine(ResetBall(currentVelocity));
+        }
+
+        Vector3 speed = rb.velocity.normalized * 10;
+
+        rb.velocity = Vector3.Lerp(rb.velocity, speed, Time.deltaTime * 10);
+
         if (transform.position.z > player1.position.z)
         {
             Debug.Log("Player2 Scored");
-            SpawnBall();
+            StartCoroutine(Pause());
         }
 
         if (transform.position.z < player2.position.z)
         {
             Debug.Log("Player1 Scored");
-            SpawnBall();
+            StartCoroutine(Pause());
         }
     }
 
-    void SpawnBall()
+    private void SpawnBall()
     {
-        transform.position = new Vector3(0, 0.5f, Random.Range(-4, -6));
+        transform.position = new Vector3(0, 0.5f, -5);
+    }
+
+    private IEnumerator Pause()
+    {
+        SpawnBall();
+
+        rb.velocity = new Vector3(0, 0, 0);
+
+        yield return new WaitForSeconds(2);
+
+        float z = Random.Range(-1, 2);
+
+        if (z == 0)
+            z = 1;
+
+        rb.velocity = new Vector3(0, 0, 6 * z);
+    }
+
+    private IEnumerator ResetBall(Vector3 currentVelocity)
+    {
+        yield return new WaitForSeconds(2);
+        float z = Random.Range(-1, 2);
+
+        if (z == 0)
+            z = 1;
+
+        rb.velocity = new Vector3(0, 0, 6 * z);
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag == "Player1")
+        {
+            if(p1.velocity.y > 0)
+                rb.velocity = new Vector3(0, -6, -6);
+
+            if (p1.velocity.x > 0)
+                rb.velocity = new Vector3(-6, 0, -6);
+        }
+
+        if (hit.gameObject.tag == "Player2")
+        {
+            if (p2.velocity.y > 0)
+                rb.velocity = new Vector3(0, 6, 6);
+
+            if (p2.velocity.x > 0)
+                rb.velocity = new Vector3(6, 0, 6);
+        }
     }
 }
--- FunProject/Assets/Scripts/PlayerController.cs
+++ FunProject/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public sealed class PlayerController : MonoBehaviour {
 
+
+    private static PlayerController instance;
+
+    public static PlayerController self
+    {
+        get
+        {
+            if (instance == null)
+                instance = new PlayerController();
+            return instance;
+        }
+    }
     [SerializeField]
     private Transform player1;
     [SerializeField]
+    [Range(6.0f, 10.0f)]
+    private float p1Speed;
+
+    [SerializeField]
     private Transform player2;
+    [SerializeField]
+    [Range(6.0f,10.0f)]
+    private float p2Speed;
 
     private Vector3 Up;
     private Vector3 Down;
@@ -32,6 +51,9 @@ public class PlayerController : MonoBehaviour {
 
         pos1 = player1.position;
         pos2 = player2.position;
+
+        Mathf.Clamp(p1Speed, 5, 10);
+        Mathf.Clamp(p2Speed, 5, 10);
     }
 	
 	// Update is called once per frame
@@ -43,28 +65,31 @@ public class PlayerController : MonoBehaviour {
 
     private void Player1Movement()
     {
+        player1.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player2.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
             pos1.y = Mathf.Clamp(player1.position.y, 0.5f, 9.5f);
-            pos1 += Up * Time.deltaTime * 2.5f;
+            pos1 += Up * Time.deltaTime * p1Speed;
             player1.position = pos1;
         }
         if (Input.GetKey(KeyCode.A))
         {
             pos1.x = Mathf.Clamp(player1.position.x, -4.5f, 4.5f);
-            pos1 += Left * Time.deltaTime * 2.5f;
+            pos1 += Left * Time.deltaTime * p1Speed;
             player1.position = pos1;
         }
         if (Input.GetKey(KeyCode.S))
         {
             pos1.y = Mathf.Clamp(player1.position.y, 0.5f, 9.5f);
-            pos1 += Down * Time.deltaTime * 2.5f;
+            pos1 += Down * Time.deltaTime * p1Speed;
             player1.position = pos1;
         }
         if (Input.GetKey(KeyCode.D))
         {
             pos1.x = Mathf.Clamp(player1.position.x, -4.5f, 4.5f);
-            pos1 += Right * Time.deltaTime * 2.5f;
+            pos1 += Right * Time.deltaTime * p1Speed;
             player1.position = pos1;
         }
     }
@@ -74,28 +99,27 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             pos2.y = Mathf.Clamp(player2.position.y, 0.5f, 9.5f);
-            pos2 += Up * Time.deltaTime * 2.5f;
+            pos2 += Up * Time.deltaTime * p2Speed;
             player2.position = pos2;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             pos2.x = Mathf.Clamp(player2.position.x, -4.5f, 4.5f);
-            pos2 += Left * Time.deltaTime * 2.5f;
+            pos2 += Left * Time.deltaTime * p2Speed;
             player2.position = pos2;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             pos2.y = Mathf.Clamp(player2.position.y, 0.5f, 9.5f);
-            pos2 += Down * Time.deltaTime * 2.5f;
+            pos2 += Down * Time.deltaTime * p2Speed;
             player2.position = pos2;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             pos2.x = Mathf.Clamp(player2.position.x, -4.5f, 4.5f);
-            pos2 += Right * Time.deltaTime * 2.5f;
+            pos2 += Right * Time.deltaTime * p2Speed;
             player2.position = pos2;
         }
 
     }
-
 }
